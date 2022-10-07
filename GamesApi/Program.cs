e7ad63c5a7827dbcd6cbc7ce5a4c6823ddbdf064
KirillKalutsky <kefirchik.2000@mail.ru>
2022-10-07 13:18:45 +0500

    запуск свагера в контейнере

--- GamesApi/Program.cs
+++ GamesApi/Program.cs
@@ -61,11 +61,8 @@ builder.Services.AddScoped<DeveloperService>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
